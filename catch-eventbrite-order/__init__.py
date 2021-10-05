3449587a44613f43228056fec026be2ee3549302
Chris Sharp <sharpchris@gmail.com>
2021-10-04 22:24:25 -0400

    renamed function and async posting to Power Automate

---
+++ catch-eventbrite-order/__init__.py
@@ -0,0 +1,62 @@
+import logging, os, httpx, asyncio
+from typing import List
+
+import azure.functions as func
+
+async def post_emails(emails: List[str]) -> None:
+    # Post the emails to Power Automate for adding to the Team
+    p_url = os.environ['powerAutomateURL']
+    p_params = {
+        "api-version":"2016-06-01",
+        "sv":"1.0",
+        "sp":"/triggers/manual/run",
+        "sig": os.environ['powerAutomateSig']
+    }
+    for email in emails:
+        data = {"email": email}
+        async with httpx.AsyncClient() as client:
+            try:
+                # Would make this async if I though people were adding more than one attendee per order
+                p = await client.post(p_url, json=data, params=p_params)
+                p.raise_for_status()
+            except:
+                # Could catch errors in adding people to the Team
+                # a 5xx would be caught here if the attendee's email address wasn't in the company's Active Directory
+                client.aclose()
+        
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    # Catch the webhook sent by Eventbrite for a new Order being placed
+    logging.info('Python HTTP trigger function processed a request.')
+    try:
+        req_body: List = req.get_json()
+        url: str = req_body['api_url']
+    except:
+        print("Could not find 'api_url' in the request body")
+
+        return func.HttpResponse(
+            f"Did not find the api_url",
+            status_code=400
+        )
+
+    # Get the Attendees listed on the received Eventbrite Order
+    headers = {"Authorization": f"Bearer {os.environ['eventbriteToken']}"}
+    # Append "attendees" to the Eventbrite API URL simplify getting the attendees from the Order
+    url += "attendees"
+    r = httpx.get(url, headers=headers)
+
+    # Get the emails of the attendees in the Eventbrite Order
+    emails: List[str] = []
+    for attendee in r.json()['attendees']:
+        emails.append(attendee['profile']['email'])
+
+    # Async post the emails
+    asyncio.run(post_emails(emails))
+            
+    # Respond to Eventbrite to let them know the webhook address is still working
+    return func.HttpResponse(
+        f"Found the api_url {url}\nThe email(s) posted were {emails}",
+        status_code=201,
+    )
+
